2a7fecc6acaeb63ac91520bcedff1dc0fde98f9b
J-Murage <90998532+J-Murage@users.noreply.github.com>
2022-06-19 22:27:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Python 3 code to find sum
+arr = []
+
+# input values to list
+arr = [1, 2, 3, 4, 10, 11]
+
+# sum() is an inbuilt function
+ans = sum(arr)
+
+# display sum
+print('Sum of the array is ', ans)
